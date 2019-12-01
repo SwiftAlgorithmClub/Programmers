c0a6f1215d9ae0523ebcf7ae58dfda8d09641e10
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-02 00:44:37 +0900

    쇠막대기 Lv2 스택배열 미사용 문제풀이

--- applebuddy.playground/Pages/쇠막대기_Lv2.xcplaygroundpage/Contents.swift
+++ applebuddy.playground/Pages/쇠막대기_Lv2.xcplaygroundpage/Contents.swift
@@ -1,5 +1,26 @@
 
-// MARK: - 쇠막대기
+// MARK: 쇠막대기
+
+// MARK: - 스택 미사용 풀이답안
+
+import Foundation
+
+func solution2(_ arrangement: String) -> Int {
+    let arr = Array(arrangement)
+    var Ans = 0
+    var depth = 0
+    for i in arr.indices {
+        if arr[i] == "(" { depth += 1 }
+        else {
+            depth -= 1
+            if arr[i-1] == "(" { Ans += depth }
+            else { Ans += 1 }
+        }
+    }
+    return Ans
+}
+
+// MARK: - 스택배열 사용 풀이 답안
 
 import Foundation
 
