0eb5cf2dd7f931ce3b498bf79b444ef14f396c98
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-02 23:52:01 +0900

    예산 Lv1 문제풀이

---
+++ applebuddy.playground/Pages/예산_Lv1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+
+// MARK: - 예산
+
+import Foundation
+
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    var arr = d.sorted()
+    var Ans = 0
+    var budget = budget
+    for i in arr.indices {
+        if budget >= arr[i] { budget -= arr[i]; Ans += 1 }
+        else { break }
+    }
+    return Ans
+}
