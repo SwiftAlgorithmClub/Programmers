86d8238756d8539d3e0affa173ee6807624c8c2b
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-05 01:29:25 +0900

    스티커모으기(2) Lv4 문제풀이

---
+++ applebuddy.playground/Pages/스티커모으기(2)_Lv4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+// MARK: - 스티커모으기(2) Lv4
+
+import Foundation
+
+func solution(_ sticker:[Int]) -> Int{
+    if sticker.count == 1 { return sticker[0] }
+    var answer = 0, maxSum = 0, maxSum2 = 0
+    var prev = 0, twoPrev = 0
+    
+    for i in 0..<sticker.count-1 {
+        maxSum = max(twoPrev + sticker[i], prev)
+        twoPrev = prev
+        prev = maxSum
+        answer = answer < maxSum ? maxSum : answer
+    }
+    
+    prev = 0
+    twoPrev = 0
+    for i in 1..<sticker.count {
+        maxSum2 = max(twoPrev + sticker[i], prev)
+        twoPrev = prev
+        prev = maxSum2
+        answer = answer < maxSum2 ? maxSum2 : answer
+    }
+
+    return answer
+}
