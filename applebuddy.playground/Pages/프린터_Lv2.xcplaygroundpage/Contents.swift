5d9cc01370c93455c858fbd7768a85dff3c23501
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-16 19:26:57 +0900

    프린터 Lv2 문제풀이

---
+++ applebuddy.playground/Pages/프린터_Lv2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+// MARK: 프린터 Lv2
+
+import Foundation
+
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var maxQueue = priorities.sorted()
+    var pArr = [(Int,Int)]()
+    for i in priorities.indices { pArr.append((i,priorities[i])) }
+    
+    var Ans = 0
+    while(!maxQueue.isEmpty) {
+        if pArr.first!.1 == maxQueue.last! {
+            Ans += 1
+            if pArr.first!.0 == location { break }
+            else {
+                pArr.removeFirst()
+                maxQueue.removeLast()
+            }
+        } else {
+            var front = pArr.first!
+            pArr.removeFirst()
+            pArr.append(front)
+        }
+    }
+    
+    return Ans
+}
