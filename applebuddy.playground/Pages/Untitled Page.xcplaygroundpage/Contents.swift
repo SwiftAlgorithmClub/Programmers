45102e43caa0c8bdfb7e6cfda781dc8ffaadaa55
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-18 05:21:15 +0900

    H-Index Lv2 문제풀이

---
+++ applebuddy.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+
+// MARK: H-Index Lv2
+
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    let citat = citations.sorted()
+    for H in stride(from: 1000, to: 0, by: -1) {
+        var qCnt = 0
+        var flag = true
+        for j in citat.indices {
+            if H <= citat[j] { qCnt += 1 }
+            else {
+                if citat[j] > H {
+                    flag = false
+                    break
+                }
+            }
+        }
+        if H <= qCnt && flag { return H }
+    }
+    
+    return 0
+}
+
