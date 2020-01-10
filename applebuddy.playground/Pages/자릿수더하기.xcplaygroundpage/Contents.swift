f0ddd54ddac1d1b8cb60f19ec5be2b9684868e9a
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-10 23:10:31 +0900

    자릿수더하기 Lv1 문제풀이

---
+++ applebuddy.playground/Pages/자릿수더하기.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+
+// MARK: 자릿수더하기 문제풀이
+
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var N = n, Ans = 0
+    while(N > 0) {
+        Ans += N % 10
+        N /= 10
+    }
+    return Ans
+}
